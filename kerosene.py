c6c96f6bb12e0525f30bb1b1d38a8ff72b307451
ErrantBot <errantbot@tutanota.com>
2017-02-12 16:59:18 +0000

    minor fixes and improvements

--- kerosene.py
+++ kerosene.py
@@ -117,7 +117,7 @@ import numpy as np
 from openpyxl import Workbook
 
 # Constants --------------------------------------------------------------------
-
+# todo: put a centralised version number constant
 YEARS = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025,
          2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034, 2035, 2036, 2037,
          2038, 2039, 2040, 2041, 2042, 2043, 2044, 2045, 2046, 2047, 2048, 2049,
@@ -2898,7 +2898,7 @@ class Gui(tk.Frame):
 if __name__ == "__main__":
     root = tk.Tk()
     root.resizable(width=False, height=False)
-    root.title("Kerosene 1.0.0")
+    root.title("Kerosene 1.0.1")
     kerosene = Gui(root)
     root.config(menu=kerosene.top)
     kerosene.pack(expand='yes', fill='both')
